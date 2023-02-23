86a67229fd6a30dce470e0b349c07f08c9879149
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-02-22 17:41:34 -0700

    Add health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::response::IntoResponse;
+use axum::{routing::get, Router};
+use hyper::StatusCode;
+use std::error::Error;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    axum::Server::bind(&addr)
+        .serve(app().into_make_service())
+        .await?;
+
+    Ok(())
+}
+
+fn app() -> Router {
+    Router::new().route("/health_check", get(health_check))
+}
+
+async fn health_check() -> impl IntoResponse {
+    StatusCode::OK
 }
