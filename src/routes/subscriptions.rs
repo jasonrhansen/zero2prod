2226456f35bff39d6e23206e9dd59eb0acb3ff97
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-02-25 19:35:26 -0700

    Add tracing spans to subscribe

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,7 +2,7 @@ use axum::{debug_handler, extract::State, response::IntoResponse, Form};
 use chrono::Utc;
 use hyper::StatusCode;
 use serde::Deserialize;
-use tracing::{error, info};
+use tracing::{error, info, info_span, Instrument};
 use uuid::Uuid;
 
 use crate::app_state::AppState;
@@ -18,11 +18,16 @@ pub async fn subscribe(
     State(state): State<AppState>,
     Form(form): Form<SubscriptionFormData>,
 ) -> impl IntoResponse {
-    info!(
-        "Adding '{}' '{}' as a new subscriber",
-        form.email, form.name
+    let request_id = Uuid::new_v4();
+    let subscribe_span = info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
-    info!("Saving new subscriber details in the database");
+    let _subscribe_span_guard = subscribe_span.enter();
+
+    let query_span = info_span!("Saving new subscriber details in the database");
     let result = sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -34,6 +39,7 @@ pub async fn subscribe(
         Utc::now()
     )
     .execute(&state.connection_pool)
+    .instrument(query_span)
     .await;
 
     if let Err(e) = result {
