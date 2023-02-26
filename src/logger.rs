cc9f83fa17f59fddc0436816d06c21aaa2b9d32b
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-02-25 19:39:00 -0700

    Add tracing_log

--- src/logger.rs
+++ src/logger.rs
@@ -3,8 +3,12 @@ use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::filter::LevelFilter;
 use tracing_subscriber::prelude::__tracing_subscriber_SubscriberExt;
 use tracing_subscriber::{EnvFilter, Registry};
+use tracing_log::LogTracer;
 
 pub fn initialize() {
+    // Redirect all log events to tracing subscriber.
+    LogTracer::init().expect("Failed to set logger");
+
     let filter_layer = EnvFilter::builder()
         .with_default_directive(LevelFilter::INFO.into())
         .from_env_lossy();
