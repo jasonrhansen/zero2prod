b5093bf0fa62734062ff348217dd26e92925152d
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-02-26 19:18:20 -0700

    Configuration and docker changes

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::net::TcpListener;
+use std::{net::TcpListener, time::Duration};
 
 use axum::BoxError;
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::{app_state::AppState, configuration::get_configuration, startup::run, telemetry};
 
 #[tokio::main]
@@ -12,10 +12,11 @@ async fn main() -> Result<(), BoxError> {
 
     let config = get_configuration().expect("Failed to read configuration");
 
-    let connection_pool =
-        PgPool::connect_lazy(&config.database.connection_string().expose_secret())
-            .expect("Failed to create Postgres connection pool");
-    let address = format!("127.0.0.1:{}", config.application_port);
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(Duration::from_secs(2))
+        .connect_lazy(&config.database.connection_string().expose_secret())
+        .expect("Failed to create Postgres connection pool");
+    let address = format!("{}:{}", config.application.host, config.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, AppState { connection_pool })?.await?;
 
