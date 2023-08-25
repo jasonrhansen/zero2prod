2262c9263d1884a5cf38c390830d6a5908df0093
Jason Rodney Hansen <jasonrodneyhansen@gmail.com>
2023-08-24 19:33:36 -0600

    Remove quickcheck and upgrade dev dependencies

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -35,13 +35,6 @@ mod tests {
     #[derive(Debug, Clone)]
     struct ValidEmailFixture(pub String);
 
-    impl quickcheck::Arbitrary for ValidEmailFixture {
-        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
-            let email = SafeEmail().fake_with_rng(g);
-            Self(email)
-        }
-    }
-
     #[test]
     fn empty_string_is_rejected() {
         let email = "".to_string();
@@ -60,8 +53,11 @@ mod tests {
         assert_err!(SubscriberEmail::parse(email));
     }
 
-    #[quickcheck_macros::quickcheck]
-    fn valid_emails_are_parsed_successfully(valid_email: ValidEmailFixture) -> bool {
-        SubscriberEmail::parse(valid_email.0).is_ok()
+    #[test]
+    fn valid_emails_are_parsed_successfully() {
+        for _ in 0..100 {
+            let email = SafeEmail().fake();
+            SubscriberEmail::parse(email).expect("Parsed email should be valid: {email}");
+        }
     }
 }
